8ac809d6d191326b892c20d9bd4e3bc6e100e2d0
kpetow <92036249+kpetow@users.noreply.github.com>
2023-10-23 19:28:45 -0500

    basic layout setup for now

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../../app/globals.css'
+import { FaRunning } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <header className='flex items-center justify-between space-x-2 font-bold px-10 py-5'>
+          <div className='flex items-center space-x-2'>
+            <Link href="/">
+                <FaRunning 
+                    className='cursor-pointer'
+                    size={30}
+                />
+            </Link>
+            <h1>Kevin Petow&apos;s Running Log</h1>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <Link href="/posts">
+                Posts
+            </Link>
+            <Link href="/about">
+                About
+            </Link>
+          </div>
+      </header>
+        {children}
+      </body>
     </html>
   )
 }
